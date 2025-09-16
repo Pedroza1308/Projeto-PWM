297dcea60956261796d8cf9cb84fa80b5a111133
pablo felipe <pabloletiane@gmail.com>
2025-09-16 19:28:52 -0300

    Remover arquivos SVG e adicionar componente Header com estilo

--- projeto-pwm/src/app/page.js
+++ projeto-pwm/src/app/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
-
+import Header from "../components/Header";
 export default function Home() {
   return (
     <div>
       <h1>Sejam bem vindos ao nosso site
         <tag></tag>
       </h1>
+      <Header/>
     </div>
   );
 }
