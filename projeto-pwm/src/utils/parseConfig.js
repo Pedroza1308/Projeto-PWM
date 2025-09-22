346a33dfdd3d92d2f3b13fde110db69a4e4df663
Nunno Wakiyama Diniz Carvalho <142551010+nunnowdc@users.noreply.github.com>
2025-09-22 21:08:26 +0000

    parse install + llink com back4app

---
+++ projeto-pwm/src/utils/parseConfig.js
@@ -0,0 +1,13 @@
+import Parse from 'parse';
+
+// Suas chaves de API do Back4app
+const APP_ID = 'Bc7Oo5on0izZKr4Zr8GJWr6kZ1v9z0N3B70vRBai';
+const JAVASCRIPT_KEY = 'rAek0mmuYbQ34Lkaihp01k2EHH5hL3TH4Wx9yTCm';
+const SERVER_URL = 'https://parseapi.back4app.com/';
+
+if (!Parse.isInitialized()) {
+  Parse.initialize(APP_ID, JAVASCRIPT_KEY);
+  Parse.serverURL = SERVER_URL;
+}
+
+export default Parse;
