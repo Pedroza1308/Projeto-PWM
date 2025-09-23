3b490ce084136baa07064dce54a8e304ee5e295c
Pedro_Alves <132352503+Pedroza1308@users.noreply.github.com>
2025-09-23 00:07:12 +0000

    substituindo a KEYS

--- projeto-pwm/src/utils/parseConfig.js
+++ projeto-pwm/src/utils/parseConfig.js
@@ -1,11 +1,16 @@
-import * as Parse from 'parse';
+import Parse from 'parse';
 
 // Suas chaves de API do Back4app
-const APP_ID = 'Bc7Oo5on0izZKr4Zr8GJWr6kZ1v9z0N3B70vRBai'; 
-const JAVASCRIPT_KEY = 'rAek0mmuYbQ34Lkaihp01k2EHH5hL3TH4Wx9yTCm';
-const SERVER_URL = 'https://parseapi.back4app.com';
+const APP_ID = 'mtZbVG1Y9x9ennJXtn9VTlJUDLPHO4c4LLmbel4t'; 
+const JAVASCRIPT_KEY = 'Z6vmCFqNPW6Ntj2cgbDK2xNpUeLgH3xvJa6yZBZ0';
 
-// Inicializa o Parse, passando a URL do servidor como uma opção
-Parse.initialize(APP_ID, JAVASCRIPT_KEY, { serverURL: SERVER_URL });
+// CORREÇÃO: Adicionada a barra "/" no final da URL
+const SERVER_URL = 'https://parseapi.back4app.com/';
+
+// CORREÇÃO: A inicialização agora é feita em duas linhas separadas
+Parse.initialize(APP_ID, JAVASCRIPT_KEY);
+Parse.serverURL = SERVER_URL;
 
 export default Parse;
+
+
